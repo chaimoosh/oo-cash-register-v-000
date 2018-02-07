69f36484987ee3870ab183bcc57db09b404049e7
Chaim Reisman <crreisman@gmail.com>
2018-02-07 14:57:08 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+require'pry'
 
+class CashRegister
+  attr_accessor :total 
+  
+  def initialize
+    @total = 0
+  end
+  
+  def self.create
+    cash_register = self.new 
+    @total = 0
+  end 
+    
+end 
