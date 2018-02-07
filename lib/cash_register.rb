000f115ef0520d768d368e1c0ca745b89656adb8
Chaim Reisman <crreisman@gmail.com>
2018-02-07 15:00:29 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,11 +4,10 @@ class CashRegister
   attr_accessor :total 
   
   def initialize
-    @total = 0
+    #@total = 0
   end
   
   def self.create
-    cash_register = self.new 
     @total = 0
   end 
     
